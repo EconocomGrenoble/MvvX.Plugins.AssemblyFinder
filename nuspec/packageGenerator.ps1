5890ae2ed494ca5683b4ab53995d0029f5d6becb
MACK Mathieu <mathieu.mack@LIVE.FR>
2017-01-05 22:27:36 +0100

    Fix nuspec file for UWP

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -1,3 +1,8 @@
+write-host "**************************" -foreground "Cyan"
+write-host "*   Packaging to nuget   *" -foreground "Cyan"
+write-host "**************************" -foreground "Cyan"
+
+#$location  = "C:\Sources\NoSqlRepositories";
 $location  = $env:APPVEYOR_BUILD_FOLDER
 
 $locationNuspec = $location + "\nuspec"
@@ -5,25 +10,30 @@ $locationNuspec
 	
 Set-Location -Path $locationNuspec
 
-"Packaging to nuget..."
-"Build folder : " + $location
-
 $strPath = $location + '\MvvX.Plugins.AssemblyFinder\bin\Release\MvvX.Plugins.AssemblyFinder.dll'
 
 $VersionInfos = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($strPath)
 $ProductVersion = $VersionInfos.ProductVersion
-"Product version : " + $ProductVersion
+write-host "Product version : " $ProductVersion -foreground "Green"
 
-"Update nuspec versions ..."	
+write-host "Packaging to nuget..." -foreground "Magenta"
+
+write-host "Update nuspec versions" -foreground "Green"
+
+write-host "Update nuspec versions MvvX.Plugins.AssemblyFinder.nuspec" -foreground "DarkGray"
 $nuSpecFile =  $locationNuspec + '\MvvX.Plugins.AssemblyFinder.nuspec'
 (Get-Content $nuSpecFile) | 
 Foreach-Object {$_ -replace "{BuildNumberVersion}", "$ProductVersion" } | 
 Set-Content $nuSpecFile
 
-"Generate nuget package ..."
+write-host "Generate nuget packages" -foreground "Green"
+
+write-host "Generate nuget package for MvvX.Plugins.AssemblyFinder.nuspec" -foreground "DarkGray"
 .\NuGet.exe pack MvvX.Plugins.AssemblyFinder.nuspec
 
-#$apiKey = $env:NuGetApiKey
+$apiKey = $env:NuGetApiKey
 	
-"Publish packages ..."	
-#.\NuGet push MvvX.Plugins.AssemblyFinder.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
+write-host "Publish nuget packages" -foreground "Green"
+
+write-host MvvX.Plugins.AssemblyFinder.$ProductVersion.nupkg -foreground "DarkGray"
+.\NuGet push MvvX.Plugins.AssemblyFinder.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
