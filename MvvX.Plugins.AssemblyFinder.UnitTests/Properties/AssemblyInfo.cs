bbb0e39b0f0e32ece4fb23e15aa77f7ea8d4d879
MACK Mathieu <mathieu.mack@econocom.com>
2019-03-14 21:04:29 +0100

    Update unit tests for appveyor build

---
+++ MvvX.Plugins.AssemblyFinder.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MvvX.Plugins.AssemblyFinder.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvvX.Plugins.AssemblyFinder.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7ebd4649-c637-4609-95ff-90d2444f925f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
